0f4e0ccbe29e7505a657e38eeffbc221f394d4db
Brandon McKinzie <bsmckinzie@lbl.gov>
2015-07-09 18:01:27 -0400

    added some more stuff ya know.

--- file_processing/get_jackknife_means.cxx
+++ file_processing/get_jackknife_means.cxx
@@ -34,15 +34,15 @@ void get_jackknife_means()
     Float_t jth_C3_u[nTimes][nFiles][2];
     Float_t jth_C3_d[nTimes][nFiles][2];
 
-    JackHistGroup jackHists;
     CorrelationTrees * CTrees = new CorrelationTrees();
     Data C2_data[nSources]; 
     Data C3_data[nSources];
     Data avg_C2_data;
     Data avg_C3_data;
 
+    // ===== initialize source-specific trees that hold input data =====
+
     TString tupleName;
-    // initialize source-specific trees that hold input data
     for (Int_t src = 0; src < nSources; src++)
     {
         tupleName = "C2_src"; 
@@ -53,39 +53,34 @@ void get_jackknife_means()
         tupleName += 8*src;
         CTrees->SetC3Tree(src, (TTree*)f->Get(tupleName.Data()));
     }
-        
     CTrees->SetBranchAddresses(C2_data, C3_data);
+    // ----------------------------------------------------------------
 
-    Int_t timeslice, count(0);
+    // ================== perform source average ==================
+
+    Int_t timeslice, fcount(0);
     Int_t nEntries = (Int_t)CTrees->GetC3Tree(0)->GetEntries();
-    cout << nEntries << endl;
-    
-    // perform source average
     for (int i = 0; i < nEntries; i++)
     {
         timeslice = i%nTimes;
-
         avg_C2_data = CTrees->SourceAveragedData(C2_data, 2, i);
         avg_C3_data = CTrees->SourceAveragedData(C3_data, 3, i);
-        avg_C2_data.Print();
-    }
-/*
+    
         if (timeslice == 0 && i !=0)
         {
             fcount++;
         }
 
-        C2_time_file[timeslice][count][0]   = C2_data.R1;
-        C2_time_file[timeslice][count][1]   = C2_data.I1;
-        C3_u_time_file[timeslice][count][0] = C3_data.R1;
-        C3_u_time_file[timeslice][count][1] = C3_data.I1;
-        C3_d_time_file[timeslice][count][0] = C3_data.R2;
-        C3_d_time_file[timeslice][count][1] = C3_data.I2;
-
-        }
+        C2_time_file[timeslice][fcount][0]   = avg_C2_data.R1();
+        C2_time_file[timeslice][fcount][1]   = avg_C2_data.I1();
+        C3_u_time_file[timeslice][fcount][0] = avg_C3_data.R1();
+        C3_u_time_file[timeslice][fcount][1] = avg_C3_data.I1();
+        C3_d_time_file[timeslice][fcount][0] = avg_C3_data.R2();
+        C3_d_time_file[timeslice][fcount][1] = avg_C3_data.I2();
     }
+    // -----------------------------------------------------------
 
-    
+    JackHistGroup * jackHists = new JackHistGroup();
     for (int time = 0; time < nTimes; time++)
     {
             for (int j = 0; j < nFiles; j++)
@@ -120,16 +115,16 @@ void get_jackknife_means()
                 jth_C3_d[time][j][0] = jth_C3_d[time][j][0] / Float_t(nFiles -1);
                 jth_C3_d[time][j][1] = jth_C3_d[time][j][1] / Float_t(nFiles -1);
 
-                jth_C2_hist[j][0]->SetBinContent(time+1, jth_C2[time][j][0]);
-                jth_C2_hist[j][1]->SetBinContent(time+1, jth_C2[time][j][1]);
+                jackHists->GetC2(j,"real")->SetBinContent(time+1, jth_C2[time][j][0]);
+                jackHists->GetC2(j,"imag")->SetBinContent(time+1, jth_C2[time][j][1]);
 
-                jth_C3_u_hist[j][0]->SetBinContent(time+1, jth_C3_u[time][j][0]);
-                jth_C3_u_hist[j][1]->SetBinContent(time+1, jth_C3_u[time][j][1]);
-                jth_C3_d_hist[j][0]->SetBinContent(time+1, jth_C3_d[time][j][0]);
-                jth_C3_d_hist[j][1]->SetBinContent(time+1, jth_C3_d[time][j][1]);
+                jackHists->GetC3_up(j,"real")->SetBinContent(time+1, jth_C3_u[time][j][0]);
+                jackHists->GetC3_up(j,"imag")->SetBinContent(time+1, jth_C3_u[time][j][1]);
+                jackHists->GetC3_down(j,"real")->SetBinContent(time+1, jth_C3_d[time][j][0]);
+                jackHists->GetC3_down(j,"imag")->SetBinContent(time+1, jth_C3_d[time][j][1]);
             }
     }
 
     outFile->Write();
-*/
+
 }
